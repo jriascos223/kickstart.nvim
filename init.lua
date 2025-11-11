9062d62f404fa16d51879ab26bd47cdb3133d34b
jriascos223 <josephriascos@gmail.com>
2025-11-10 22:39:07 -0500

    ktlint formatting args

--- init.lua
+++ init.lua
@@ -693,11 +693,18 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        kotlin = { 'ktlint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        kotlin = {
+          args = { '-F', vim.api.nvim_buf_get_name(0) },
+          stdin = false,
+        },
       },
     },
   },
